4961dbca6e478c7c6588f2cb283a95dce43b2e58
2705DeeptiN <deeptinidoni07@gmail.com>
2026-08-22 10:47:08 +0530

    Add: climbing-stairs - submission-6

---
+++ Data Structures & Algorithms/climbing-stairs/submission-6.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+
+        return solve(n, dp);
+    }
+    public int solve(int i, int[] dp) {
+        if (i== 0)
+            return 0;
+        if (i == 1)
+            return 1;
+        if (i == 2)
+            return 2;
+
+        if (dp[i] != -1)
+            return dp[i];
+
+        return dp[i] = solve(i - 1,dp) + solve(i - 2,dp);
+    }
+}
