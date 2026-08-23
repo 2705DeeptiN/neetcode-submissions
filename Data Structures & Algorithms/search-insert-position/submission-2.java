cda45f97b2e9b4d14480d52f76d31913c6528913
2705DeeptiN <deeptinidoni07@gmail.com>
2026-08-23 11:19:27 +0530

    Add: search-insert-position - submission-2

---
+++ Data Structures & Algorithms/search-insert-position/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int searchInsert(int[] arr, int target) {
+        int n=arr.length;
+
+        int lo=0;
+        int hi=n-1;
+
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+
+            if(arr[mid]<target) lo=mid+1;
+            else hi=mid-1;
+        }
+        return lo;
+    }
+}
