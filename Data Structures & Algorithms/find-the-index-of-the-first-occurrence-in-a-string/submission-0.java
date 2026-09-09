2566520a1ccd7d6500703eb5bcf2f0f40869871a
2705DeeptiN <deeptinidoni07@gmail.com>
2026-09-09 20:33:33 +0530

    Add: find-the-index-of-the-first-occurrence-in-a-string - submission-0

---
+++ Data Structures & Algorithms/find-the-index-of-the-first-occurrence-in-a-string/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int strStr(String s1, String s2) {
+        int l1=s1.length();
+        int l2=s2.length();
+
+        for(int i=0;i<=l1-l2;i++){
+            int j=0;
+
+            while(j<l2 && s1.charAt(i+j)==s2.charAt(j)) j++;
+
+            if(j==l2) return i;
+        }
+        return -1;
+    }
+}
